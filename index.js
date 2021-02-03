0c37575c21e6bff2b5dcbbf8d357a322f633e089
Maxim Kirshin <meowto16@gmail.com>
2021-02-04 01:29:05 +0500

    simple web-server with html pages

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    res.writeHead(400, {
-      'Content-Type': 'text/html',
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
     })
 
-    res.end(`
-      <h1>Form</h1>
-      <form method="POST" action="/">
-        <input name="title" type="text" />
-        <button type="submit">Submit</button>
-      </form>
-    `)
+    switch (req.url) {
+      case '/':
+        return fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
+          if (err) throw err
+          res.end(content)
+        })
+      case '/about':
+        return fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
+          if (err) throw err
+          res.end(content)
+        })
+    }
+
   } else if (req.method === 'POST') {
     const body = []
     res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8',
+      'Content-Type': 'text/html; charset=utf-8'
 
     })
 
