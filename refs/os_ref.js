bb718c0edca3938fafd43e299527342f5811f2bf
Maxim Kirshin <meowto16@gmail.com>
2021-02-04 00:15:24 +0500

    OS module (Operational System)

---
+++ refs/os_ref.js
@@ -0,0 +1,24 @@
+const os = require('os')
+
+// Платформа
+console.log(os.platform())
+
+// Архитектура
+console.log(os.arch())
+
+// Информация
+console.log(os.cpus())
+
+// Свободная память
+console.log(os.freemem())
+
+// Сколько всего памяти есть
+console.log(os.totalmem())
+
+// Корневая директория
+console.log(os.homedir())
+
+// Сколько система работает
+console.log(os.uptime())
+
+
