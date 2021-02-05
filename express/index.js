9acbead78bbd36c38d77f393ae562fb275b06dd9
Maxim Kirshin <meowto16@gmail.com>
2021-02-06 00:24:26 +0500

    form handling

--- express/index.js
+++ express/index.js
@@ -16,6 +16,9 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: true
+}))
 app.use('/', homeRouter)
 app.use('/add', addRouter)
 app.use('/courses', coursesRouter)
