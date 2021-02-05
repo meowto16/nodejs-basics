482ff7ea2f5660488328e00cf2ec12f110f32c42
Maxim Kirshin <meowto16@gmail.com>
2021-02-05 10:22:38 +0500

    work with html files

--- express/index.js
+++ express/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+})
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views', 'about.html'))
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`)
 })
