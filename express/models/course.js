d15b5dd0648ebee51f1ff4e4076584667f036e32
Maxim Kirshin <meowto16@gmail.com>
2021-02-06 00:51:09 +0500

    create model

---
+++ express/models/course.js
@@ -0,0 +1,48 @@
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
+const path = require('path')
+
+class Course {
+  constructor(title, price, img) {
+    this.title = title
+    this.price = price
+    this.img = img
+    this.id = uuidv4()
+  }
+
+  toJSON() {
+    return ({
+      title: this.title,
+      price: this.price,
+      img: this.img,
+      id: this.id
+    })
+  }
+
+  async save() {
+    const courses = await Course.getAll()
+    courses.push(this.toJSON())
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.resolve(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) reject(err)
+          else resolve()
+        }
+      )
+    })
+  }
+
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path.resolve(__dirname, '..', 'data', 'courses.json'), 'utf-8', (err, content) => {
+        if (err) reject(err)
+        else resolve(JSON.parse(content))
+      })
+    })
+  }
+}
+
+module.exports = Course
--- express/routes/add.js
+++ express/routes/add.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const Course = require('../models/course')
 const router = Router()
 
 router.get('/', (req, res) => {
@@ -8,8 +9,10 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post('/', async (req, res) => {
+  const { title, price, image } = req.body
+  const course = new Course(title, price, image)
+  await course.save()
   res.redirect('/courses')
 })
 
