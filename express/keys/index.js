df7e7f4b775138712e49cc0028b2cd896a5a42cb
Maxim Kirshin <meowto16@gmail.com>
2021-02-17 08:48:57 +0500

    refactor: adding keys constants

---
+++ express/keys/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: process.env.APP_PORT,
+  MONGO_DB: {
+    URL: process.env.APP_MONGO_DB_URL,
+    USER: process.env.APP_MONGO_DB_USER,
+    PASS: process.env.APP_MONGO_DB_PASS,
+  }
+}
