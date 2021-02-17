f4f4b5bca9f584d8796cafd5e0e0a793b6c04084
Maxim Kirshin <meowto16@gmail.com>
2021-02-17 09:31:14 +0500

    feature: adding registration email

--- express/keys/index.js
+++ express/keys/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.APP_PORT,
+  BASE_URL: process.env.APP_BASE_URL,
   MONGO_DB: {
     URL: process.env.APP_MONGO_DB_URL,
     USER: process.env.APP_MONGO_DB_USER,
@@ -7,5 +8,8 @@ module.exports = {
   },
   SENDGRID: {
     KEY: process.env.APP_SENDGRID_KEY
+  },
+  EMAIL: {
+   DEFAULT_FROM: process.env.APP_DEFAULT_EMAIL_FROM
   }
 }
