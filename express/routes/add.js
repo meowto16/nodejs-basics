0d8439ba03881a7a645c9c057277d45eaffc543b
Maxim Kirshin <meowto16@gmail.com>
2021-02-22 16:38:29 +0500

    feature: adding validators for add/edit courses

--- express/routes/add.js
+++ express/routes/add.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
+const { validationResult } = require('express-validator')
+
 const Course = require('../models/course')
 const auth = require('../middleware/auth')
 const router = Router()
+
+const { courseValidators } = require('../utils/validators')
 
 router.get('/', auth, (req, res) => {
   res.render('add', {
@@ -10,7 +14,19 @@ router.get('/', auth, (req, res) => {
   })
 })
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, courseValidators, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render('add', {
+      title: 'Добавить курс',
+      isAdd: true,
+      error: errors.array()[0].msg,
+      data: {
+        ...(req.body ?? {})
+      }
+    })
+  }
+
   const { title, price, img } = req.body
   const course = new Course({
     title,
