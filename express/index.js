8233549b1da15b7d7c0eaf98dadc19691926f9ab
Maxim Kirshin <meowto16@gmail.com>
2021-02-23 23:52:49 +0500

    feature: adding headers

--- express/index.js
+++ express/index.js
@@ -8,6 +8,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 const MongoStore = require('connect-mongodb-session')(session)
 
 const homeRouter = require('./routes/home')
@@ -56,6 +57,7 @@ app.use(session({
 app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
+app.use(helmet())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
